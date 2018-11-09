adb3f78fe37e3386ffeb5e58e564ee5354df6e17
yoo-minho <wingedcompany@gmail.com>
2018-11-09 21:26:32 +0900

    upload ver

--- app/src/main/java/com/uminoh/bulnati/SecretKey.java
+++ app/src/main/java/com/uminoh/bulnati/SecretKey.java
@@ -2,10 +2,10 @@ package com.uminoh.bulnati;
 
 public class SecretKey {
 
-    public static String ip = "18.182.54.93";
+    public static String ip = "";
     public static int port = 7777;
     public static String cfrUrl = "https://openapi.naver.com";
-    public static String clientId = "z8V3UoaUhXW7Qg8A_vGf";
-    public static String clientSecret = "AupOnrkpk8";
+    public static String clientId = "";
+    public static String clientSecret = "";
 
 }
